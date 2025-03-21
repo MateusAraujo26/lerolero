2a639aaa883d423def25b45dee58f103385f10e0
3G <gustavogenerosoguerra@usp.br>
2025-03-21 10:27:26 -0300

    :rocket: :art:

--- lerolero.py
+++ lerolero.py
@@ -6,7 +6,7 @@ Gera frases de efeito sem significado real."""
 
 import random
 
-parte1 = ["O sistema em desenvolvimento" , "O novo protocolo de comunicação", "O algoritmo otimizado"]
+parte1 = ["O sistema em desenvolvimento" , "O novo protocolo de comunicação", "O algoritmo de otimização"]
 parte2 = ["possui excelente desempenho", "oferece garantias de precisão acima da média", "preenche uma lacuna significativa"]
 parte3 = ["nas aplicações a que se destina", "em relação às opções disponível no mercado" , ", provendo ampla vantagem competitiva a seus usuários"]
 
